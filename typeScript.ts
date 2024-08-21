df82b553373d8d31ddbe138d05e84d274e0499b3
SandeeepS <sandeepsanthosh3303@gmail.com>
2024-08-21 09:18:01 +0530

    added changes

---
+++ typeScript.ts
@@ -0,0 +1,27 @@
+// infer types 
+
+// let userName = "sandeep";
+// userName = 10;
+
+
+//Defining types (Explicit Types)
+// let userName: string = "sandeep";
+// let number: number = 5000;
+// let isSubscribed: boolean = true;
+// let skills: string[] = ["js","TS"];
+// let count: number[] = [4,2,5];
+// let emptyArray: [] = [];
+// let userDetails: {name:string;age:number} = {
+//     name:"sandeep",
+//     age:22,
+// };
+
+//Interface
+let userDetails: {name:string;age:number;salary:number} = {
+    name:"sandeep",
+    age:22,
+    salary:60000
+};
+
+
+ 
