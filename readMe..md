fd7e2dce308c1048a9bbaa7b6ef830de977aa9a0
SandeeepS <sandeepsanthosh3303@gmail.com>
2024-08-21 11:09:23 +0530

    added a readme file and function typscript

---
+++ readMe..md
@@ -0,0 +1,12 @@
+[x] Inter types(implicit types)
+[X] Defining types (explicit types)
+[x] Interfaces
+[X] Types
+[X] Union/Optional
+[x] Funcitons
+[x] Named types
+[ ] Functions overloading 
+[ ] Generics
+[ ] Enums/as const
+[ ] Utility Types
+[ ] any/unknown/never/void
--- typeScript.ts
+++ typeScript.ts
@@ -1,10 +1,10 @@
-// infer types 
+// infer types >>>>>
 
 // let userName = "sandeep";
 // userName = 10;
 
 
-//Defining types (Explicit Types)
+//Defining types (Explicit Types) >>>>>
 // let userName: string = "sandeep";
 // let number: number = 5000;
 // let isSubscribed: boolean = true;
@@ -16,12 +16,90 @@
 //     age:22,
 // };
 
-//Interface
-let userDetails: {name:string;age:number;salary:number} = {
-    name:"sandeep",
-    age:22,
-    salary:60000
-};
+//Interface >>>>
+
+// interface Details {
+//     name:string;
+//     age:number;
+//     salary:number;
+//     getName: () => void;
+// }
+// let userDetails: Details = {
+//     name:"sandeep",
+//     age:22,
+//     salary:60000,
+//     getName() {
+//         console.log(this.name)
+//     },
+// };
 
 
+//Type >>>>
+// type Details = {
+//     name:string;
+//     age:number;
+//     salary:number;
+//     getName: () => void;
+// }
+
+// let userDetails: Details = {
+//     name:"sandeep",
+//     age:22,
+//     salary:60000,
+//     getName() {
+//         console.log(this.name)
+//     },
+// };
+
+
+// Union / Optional >>>>
+// type Details = {
+//     name:string;
+//     age:number | string;
+//     salary:number | string;
+//     getName?: () => void; // provided optional functionality
+// }
+
+// let userDetails: Details = {
+//     name:"sandeep",
+//     age:22,
+//     salary:60000,
+
+// };
+
+//Functions >>>>>
+
+// type Details = {
+//     name:string;
+//     age:number | string;
+//     salary:number | string;
+//     getName?: () => void; // provided optional functionality
+// }
+
+// let userDetails: Details = {
+//     name:"sandeep",
+//     age:22,
+//     salary:60000,
+
+// };
+
+// function getUserName(userDetails:Details):name{
+//       return userDetails.age
+// }
+
+// getUserName(userDetails);
  
+
+     // -- creting with inline
+  
+    
+    // function getUserName({name,age} : {name:string; age:number}){
+    //       return age;
+    // }
+    
+    // getUserName({name:"sandeep",age:20});
+
+// Named Types >>>>
+
+type StatusType = "pending" | "completed" | "failed";
+let currentStatus:StatusType = "pending";
